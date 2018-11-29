a0390417386783651748becd213a4812d9436ca5
AMHR <amhr_1997@hotmail.com>
2018-11-28 21:50:46 -0700

    peueba

--- controllers/cita.controller.js
+++ controllers/cita.controller.js
@@ -83,6 +83,7 @@ function Reactivar(req,res){
 function citasActuales(req,res){
     let idDoctor=req.params.idDoctor;
     let hoy = moment().format('YYYY-MM-DD');
+    hoy = String(hoy);
     Cita.find({fecha:hoy, status:'Pendiente',idDoctor:idDoctor}).populate({path:'idPaciente'}).exec((err, citasActuales)=>{
         if(err) return res.status(500).send({message: `Se presento un error innesperado ${err}`});
         if(!citasActuales) return res.status(500).send({message: `No se encontraron citas`});
