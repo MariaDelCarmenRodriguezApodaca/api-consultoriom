71b48dc77cdec0ca1cf93b660c184b363dbc2fae
AMHR <amhr_1997@hotmail.com>
2018-11-28 21:09:17 -0700

    historiaas medias

--- routes/historiaMedica.routes.js
+++ routes/historiaMedica.routes.js
@@ -10,6 +10,6 @@ const md_admin = require ('../middlewares/admin');
 
 //rutas
 router.post('/add', [autorizacion.autenticado, md_trabajador.isEmpleado], historiaMedica.addHistoriaM);
-router.get('/get',  [autorizacion.autenticado, md_trabajador.isEmpleado], historiaMedica.getHistoriasM);
-router.get('/getHistoria/:id', [autorizacion.autenticado, md_trabajador.isEmpleado], historiaMedica.getHistoriaPaciente);
+router.get('/get',  [autorizacion.autenticado], historiaMedica.getHistoriasM);
+router.get('/getHistoria/:id', [autorizacion.autenticado], historiaMedica.getHistoriaPaciente);
 module.exports = router;
